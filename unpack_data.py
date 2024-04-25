7dd71a3fd61854ad821876e36dfffa564d8e1740
Paul <p.m.rodenburg@student.tue.nl>
2024-04-25 12:05:02 +0200

    check for zips unpack_data

--- unpack_data.py
+++ unpack_data.py
@@ -3,11 +3,6 @@ import zipfile
 from tqdm import tqdm
 import shutil
 
-# Make sure data folder is empty
-if os.path.isdir("data/csv"):
-    shutil.rmtree("data/csv")
-os.mkdir("data/csv")
-
 files = ["2024-02.zip",
          "2021-02.zip",
          "2018-02.zip",
@@ -20,7 +15,12 @@ zip_files = {file for file in files_os if file.endswith('.zip')}
 files_set = set(files)
 
 if files_set != zip_files:
-    print("You don't have all the required zip files. Run download_data.py first to download all the zips.")
+    raise ValueError(f"You don't have all the required zip files. Run download_data.py first to download all the zips.\n You have: {zip_files} \n You need {files_set}")
+
+# Make sure data folder is empty
+if os.path.isdir("data/csv"):
+    shutil.rmtree("data/csv")
+os.mkdir("data/csv")
 
 for file_name in files:
     target_path = "data/csv"
