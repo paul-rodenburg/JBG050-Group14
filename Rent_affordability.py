852afe201cfee78127227978cea21cd75e14746f
Marge6969 <martha.vinograd@gmail.com>
2024-05-23 21:41:30 +0200

    split the plot into two

--- Rent_affordability.py
+++ Rent_affordability.py
@@ -11,11 +11,21 @@ df_best_worst = df_indexed.loc[["Kensington and Chelsea", "Westminster", "Sutton
 
 #turning the ratios from object to float
 df_best_worst = df_best_worst.map(lambda x: float(x.replace(',', '.')))
-print(df_best_worst)
 
-#plotting line chart
-df_best_worst.transpose().plot(kind='line', marker='o', figsize=(12, 8))
-plt.title('Property Affordability for Each Borough (2016-2022)')
+df_best = df_best_worst.loc[["Kensington and Chelsea", "Westminster", "Sutton", "Bexley", "Kingston upon Thames"]]
+df_worst = df_best_worst.loc[["Lambeth", "Islington", "Haringey", "Lewisham", "Hackney"]]
+
+
+#plotting line charts
+df_best.transpose().plot(kind='line', marker='o', figsize=(12, 8))
+plt.title('Property Affordability for Most Trusting Boroughs (2016-2022)')
+plt.xlabel('Year')
+plt.ylabel('Income to Rent Ratio')
+plt.grid(True)
+plt.legend(title='Borough')
+plt.show()
+df_worst.transpose().plot(kind='line', marker='o', figsize=(12, 8))
+plt.title('Property Affordability for Least Trusting Boroughs (2016-2022)')
 plt.xlabel('Year')
 plt.ylabel('Income to Rent Ratio')
 plt.grid(True)
