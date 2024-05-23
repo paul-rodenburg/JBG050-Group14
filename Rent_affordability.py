ac112624beacb5fd8914cb585071918b1dedd146
Marge6969 <martha.vinograd@gmail.com>
2024-05-23 23:13:18 +0200

    unemployment rate graphs

--- Rent_affordability.py
+++ Rent_affordability.py
@@ -1,7 +1,38 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+df_unemp = pd.read_csv('data/unemploymentRates.csv', delimiter = ';')
+df_unemp = df_unemp.set_index('Area')
+df_unemp = df_unemp.map(lambda x: float(x.replace(',', '.')))
+#plotting line charts
+df_unemp.loc[["Kensington and Chelsea", "Westminster", "Sutton", "Bexley", "Kingston upon Thames"]].transpose().plot(kind='line', marker='o', figsize=(12, 8))
+df_unemp.loc[["London"]].transpose().plot(kind='line', marker='o', linewidth=3, ax=plt.gca())
 
+plt.title('Unemployment Rate per Most Trusting Boroughs')
+plt.xlabel('Fiscal Year')
+plt.ylabel('Unemployment Rate')
+plt.grid(True)
+plt.legend(title='Borough')
+plt.show()
+print(df_unemp)
+
+
+'''
+
+Analysis of Unemployment Rates:
+The plots show that all boroughs experienced a dip in 
+unemployment rates in the fiscal year of 2022, except for Hackney, the lowest ranking
+borough on the trust scale. As expected, all boroughs experienced a rise in unemployment rates
+in 2020 (due to lay offs during the pandemic). The most trusting boroughs look to have a lower
+rate than London overall, showing that these top 5 boroughs are especially well-off.
+
+Overall, the most trusting boroughs have their unemployement rate 
+ranging from ~2.3 to ~6.1. (3.8)
+
+The least trusting boroughs range from ~3.5 to ~7.2. (3.7) This group has a higher unemployment
+rates which point to 
+'''
+'''
 
 df = pd.read_csv('data/ratio-house-price-earnings-residence-based.csv', delimiter = ';')
 df_indexed = df.set_index('Area')
@@ -31,6 +62,7 @@ plt.ylabel('Income to Rent Ratio')
 plt.grid(True)
 plt.legend(title='Borough')
 plt.show()
+'''
 '''
 Analysis: 
 The line plot shows that the two boroughs that have the most trust and confidence
