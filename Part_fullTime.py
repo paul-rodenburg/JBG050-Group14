9a2c00c11d89af0af1e168835cfa5f6e5d9f5d82
Marge6969 <martha.vinograd@gmail.com>
2024-05-30 21:45:13 +0200

    Update Part_fullTime.py

--- Part_fullTime.py
+++ Part_fullTime.py
@@ -1,3 +1,11 @@
-xls = pd.ExcelFile('path_to_file.xls')
-df1 = pd.read_excel(xls, 'Sheet1')
-df2 = pd.read_excel(xls, 'Sheet2')
+import pandas as pd     andas
+
+xls = pd.ExcelFile('data/employment-status-by-genderxls.xlsx')
+df1 = pd.read_excel(xls, '2016')
+df2 = pd.read_excel(xls, '2017')
+df3 = pd.read_excel(xls, '2018')
+df4 = pd.read_excel(xls, '2019')
+df5 = pd.read_excel(xls, '2020')
+df6 = pd.read_excel(xls, '2021')
+df7 = pd.read_excel(xls, '2022')
+df8 = pd.read_excel(xls, '2023')
